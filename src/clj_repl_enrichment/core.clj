58e1548f60683475649e82951ae698d11ea415ed
andersmurphy <andersmurphy@gmail.com>
2020-03-15 22:06:01 +0000

    Add function for renaming def/defn in project

---
+++ src/clj_repl_enrichment/core.clj
@@ -0,0 +1,51 @@
+(ns clj-repl-enrichment.core
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
+
+(defn get-file-name-for-ns [n]
+  (some-> n
+          ns-publics
+          first
+          second
+          meta
+          :file))
+
+(defn get-function-alias [n {:keys [name ns]}]
+  (let [namespace->alias (set/map-invert (.getAliases n))
+        alias            (namespace->alias ns)]
+    {:full-name (if alias (str alias "/" name) (str name))
+     :alias     alias}))
+
+(defn get-source-for-file [file]
+  (or (try {:source (slurp (str "src/" file))
+            :path   (str "src/" file)}
+           (catch Exception e))
+      (try {:source (slurp (str "test/" file))
+            :path   (str "test/" file)}
+           (catch Exception e))))
+
+(defn find-replace-in-source [{:keys [full-name alias new-name] :as m}]
+  (update m :source str/replace (re-pattern (str "([\\s\\[{(]+)" full-name "([\\s)}\\]]+)"))
+          (if alias
+            (str "$1" alias "/" new-name "$2")
+            (str "$1" new-name "$2"))))
+
+(defn sync-source-to-file! [{:keys [source path ns]}]
+  (spit path source)
+  (require (.getName ns) :reload))
+
+(defn rename-function-in-project [old-name new-name]
+  (let [function    (meta (resolve (symbol old-name)))
+        function-ns (:ns function)]
+    (->> (all-ns)
+         (filter #((set (vals (.getAliases %))) function-ns))
+         (concat [function-ns])
+         (map (fn [n]
+                (let [file (get-file-name-for-ns n)]
+                  (merge {:ns       n
+                          :file     file
+                          :new-name new-name}
+                         (get-source-for-file file)
+                         (get-function-alias n function)))))
+         (map find-replace-in-source)
+         (run! sync-source-to-file!))))
